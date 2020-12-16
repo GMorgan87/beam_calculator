0dade1b6d668e8a3fa4ec07e36520c31a0c6f3fc
Gmorgan87 <gmorgan87@hotmail.co.uk>
2020-12-16 12:19:57 +0000

    input form css

--- src/components/Input.js
+++ src/components/Input.js
@@ -4,33 +4,37 @@ import '../css/Input.css'
 const Input = ({inputChange, findBeam, XorY}) => {
 
   return (
-    <form className='inputs' onSubmit={findBeam}> 
-        <label>Span (L):
-            <input onChange={inputChange} type='number' name='span' required/>mm
-        </label>
-        <br/>
-        <label>Weight (W):
-            <input onChange={inputChange} type='number' name='mass' required/>kg
-        </label>
-        <br/>
-        <label>Steel Grade:
+    <form className='inputs' onSubmit={findBeam}>
+        <div className='input-wrapper'>
+          <label for="span">Span (mm):</label>
+          <input onChange={inputChange} type='number' name='span' required/>
+        </div>
+        <div className='input-wrapper'>
+          <label for="mass">Weight (kg):</label>
+          <input onChange={inputChange} type='number' name='mass' required/>
+        </div>
+        <div className='input-wrapper'>
+          <label for="grade">Steel Grade:</label>
           <input onChange={inputChange}  type="number" name="grade" defaultValue="355" className="grade-input" required/>
-        </label>
-        <br/>
-        <label>Safety Factor:
+        </div>
+        <div className='input-wrapper'>
+          <label for="safety">Safety Factor:</label>
           <input onChange={inputChange}  type="number" name="safety" defaultValue="1" steps="0.01" className="safety-input" required/>
-        </label>
-        <br/>
-        <label>Load Axis: 
+        </div>
+        <div className='defl-wrapper'>
+          <label>Allowable Deflection:</label> 
+            <div>
+              <span>L/</span><input onChange={inputChange} type="number" name="defl" defaultValue="250" className="defl-input" required/>
+            </div>
+        </div>
+        <div className='radios-wrapper'>
+          <label for="XorY">Load Axis:</label>
+          <div className="radios">
             <input onClick={inputChange} type="radio" value="x" name="XorY" checked={XorY==="x"}/>X
             <input onClick={inputChange} type="radio" value="y" name="XorY" checked={XorY==="y"}/>Y
-        </label>
-        <br/>
-        <label>Allowable Deflection: L/
-          <input onChange={inputChange} type="number" name="defl" defaultValue="250" className="defl-input" required/>
-        </label>
-        <br/>
-        <input type='submit' value="calculate"/>
+          </div>
+        </div>
+        <input type='submit' value="CALCULATE" className="calc-button"/>
     </form>
   )
 }
