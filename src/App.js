0c363f2659a80f4e69747917d61ed2bf16027cd6
Gmorgan87 <gmorgan87@hotmail.co.uk>
2021-01-15 12:54:16 +0000

    adds footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import LandingPage from './components/LandingPage'
 import CalculatorContainer from './containers/CalculatorContainer';
 
@@ -12,7 +13,6 @@ function App() {
     setStart(true)
   }
 
-
   return (
     <div className="App">
       <Header/>
@@ -21,7 +21,7 @@ function App() {
       :
       <LandingPage handleStart={handleStart}/>
       }
-      
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../css/Footer.css'
+
+const Footer = () => {
+  return (
+    <div className="footer">
+        <p>Built by <a href="http://github.com/gmorgan87">G Morgan</a> Using <a href="https://reactjs.org/">React</a></p>
+    </div>
+  )
+}
+
+export default Footer
